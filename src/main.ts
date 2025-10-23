417748e8f814d49a7457262b3711090cf77f4d58
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-23 17:37:45 +1100

    Create a box

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
 import "./style.css";
 
-document.querySelector<HTMLDivElement>("body")!.innerHTML += "Falling";
+const BODY = document.querySelector<HTMLDivElement>("body")!;
+
+function createBox(width: number, height: number) {
+  const box = document.createElement("div");
+  box.classList.add("box");
+  box.style.width = `${width}px`;
+  box.style.height = `${height}px`;
+  box.style.left = `${(window.innerWidth - width) / 2}px`;
+  box.style.top = "0px";
+  BODY.appendChild(box);
+  return box;
+}
+
+createBox(200, 50);
