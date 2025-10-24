c676b61834edfa9463adec00e73c2abbe2cf5689
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-24 22:52:37 +1100

    Spawn boxes with the spacebar

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,27 @@ import "./style.css";
 const boxManager = new BoxManager(1280, 720);
 boxManager.addBox(200, 50, undefined, 0, 0.05, "magenta");
 boxManager.addBox(800, 50, undefined, 620, 0, "white");
+
+const colors = [
+  "DeepPink",
+  "Red",
+  "Orange",
+  "Gold",
+  "Fuchsia",
+  "Lime",
+  "Turquoise",
+  "DodgerBlue",
+  "BlueViolet",
+];
+
+document.addEventListener("keydown", (event) => {
+  if (event.key != " ") return;
+  boxManager.addBox(
+    200,
+    50,
+    undefined,
+    0,
+    0.05,
+    colors[Math.floor(Math.random() * colors.length)]
+  );
+});
