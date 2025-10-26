017b4d5a89cbaf7dffb614928f8f8ecdc588b617
shock59 <42130427+shock59@users.noreply.github.com>
2025-10-26 17:54:05 +1100

    Support all box types in popup

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
 import BoxManager from "./BoxManager";
 import "./style.css";
 import audioWidget from "./widgets/audioWidget";
+import imageWidget from "./widgets/imageWidget";
+import wikipediaWidget from "./widgets/wikipediaWidget";
+import youtubeWidget from "./widgets/youtubeWidget";
 
 const boxManager = new BoxManager(1280, 720);
 
-const div = document.createElement("div");
-div.style.color = "#000000";
-div.style.fontSize = "22px";
-div.style.height = "100%";
-div.style.display = "flex";
-div.style.justifyContent = "center";
-div.style.alignItems = "center";
-div.innerText = "Floor";
-boxManager.addBox(800, 50, undefined, 620, 0, "white", [div]);
-
-boxManager.addBox(400, 50, undefined, 0, 0.08, "darkgray", [
-  audioWidget(
-    "https://upload.wikimedia.org/wikipedia/commons/f/f3/Anthem_of_Europe_%28US_Navy_instrumental_short_version%29.ogg"
-  ),
-]);
+boxManager.addBox(800, 50, undefined, 620, 0, "white");
 
 const popupBackground =
   document.querySelector<HTMLDivElement>("#popup-background")!;
@@ -30,12 +19,83 @@ popup.addEventListener("click", (event) => {
   event.stopPropagation();
 });
 
-const popupBoxColorSelect =
-  document.querySelector<HTMLSelectElement>("#color")!;
+const popupBoxTypeSelect =
+  document.querySelector<HTMLSelectElement>("#box-type")!;
+const popupBoxWidthInput =
+  document.querySelector<HTMLInputElement>("#box-width")!;
+const popupBoxHeightInput =
+  document.querySelector<HTMLSelectElement>("#box-height")!;
+const popupBoxGravityInput =
+  document.querySelector<HTMLSelectElement>("#box-gravity")!;
 const popupAddBoxButton =
   document.querySelector<HTMLButtonElement>("#add-box")!;
+
+popupBoxTypeSelect.addEventListener("change", () => {
+  for (const div of popup.querySelectorAll<HTMLDivElement>(".popupoptions")) {
+    if (div.id == `popupoptions-${popupBoxTypeSelect.value}`)
+      div.classList.remove("hidden");
+    else div.classList.add("hidden");
+  }
+});
+
 popupAddBoxButton.addEventListener("click", () => {
-  boxManager.addBox(200, 50, undefined, 0, 0.05, popupBoxColorSelect.value);
+  let widget: Element | undefined;
+
+  switch (popupBoxTypeSelect.value) {
+    case "basic":
+      const colorSelect = document.querySelector<HTMLSelectElement>("#color")!;
+      boxManager.addBox(
+        Number(popupBoxWidthInput.value),
+        Number(popupBoxHeightInput.value),
+        undefined,
+        0,
+        Number(popupBoxGravityInput.value),
+        colorSelect.value
+      );
+      break;
+
+    case "image":
+      const src = document.querySelector<HTMLInputElement>("#image-src")!;
+      widget = imageWidget(src.value);
+      break;
+
+    case "youtube":
+      const videoId = document.querySelector<HTMLInputElement>("#youtube-id")!;
+      widget = youtubeWidget(videoId.value);
+      break;
+
+    case "audio":
+      const audioSrc = document.querySelector<HTMLInputElement>("#audio-src")!;
+      widget = audioWidget(audioSrc.value);
+      break;
+
+    case "embed":
+      const embedSrc = document.querySelector<HTMLInputElement>("#embed-src")!;
+      widget = wikipediaWidget(embedSrc.value);
+      break;
+
+    case "html":
+      const customHtml =
+        document.querySelector<HTMLTextAreaElement>("#custom-html")!;
+      const div = document.createElement("div");
+      div.style.width = "100%";
+      div.style.height = "100%";
+      div.innerHTML = customHtml.value;
+      widget = div;
+      break;
+  }
+  if (widget != undefined) {
+    boxManager.addBox(
+      Number(popupBoxWidthInput.value),
+      Number(popupBoxHeightInput.value),
+      undefined,
+      0,
+      Number(popupBoxGravityInput.value),
+      "gray",
+      [widget]
+    );
+  }
+
   popupBackground.classList.add("hidden");
 });
 
